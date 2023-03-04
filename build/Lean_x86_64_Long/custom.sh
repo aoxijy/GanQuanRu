6b3f826d008d81dfb0380963f7cda6e7bacff682
Cai <106552747+aoxijy@users.noreply.github.com>
2023-03-04 18:58:59 +0800

    Update custom.sh

--- build/Lean_x86_64_Long/custom.sh
+++ build/Lean_x86_64_Long/custom.sh
@@ -9,7 +9,7 @@
 cat feeds.conf.default
 
 # 添加第三方软件包
-git clone https://github.com/aoxijy/aoxi-package package/aoxi-package
+git clone https://github.com/aoxijy/jac-package package/jac-package
 
 # 更新并安装源
 ./scripts/feeds clean
